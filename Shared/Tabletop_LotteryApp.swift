749dd096d2ab5f7b720883a731c49383688ccac2
Zach Smoak <zaguanno@gmail.com>
2021-08-23 16:34:52 -0400

    Initial Commit

---
+++ Shared/Tabletop_LotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabletop_LotteryApp.swift
+//  Shared
+//
+//  Created by Zachary Smoak on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Tabletop_LotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
